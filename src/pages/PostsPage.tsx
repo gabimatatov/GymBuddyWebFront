a3fb663e1151d8b5cbfca2b94ecb11aa04af7f7b
gternovs <gternovs@akamai.com>
2025-01-30 21:49:22 +0200

    Added footer

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Posts from "../components/Posts/Posts";
+import Footer from "../components/Footer/Footer";
 
 const PostsPage: React.FC = () => {
   return (
     <div>
       <Posts/>
+      <Footer/>
     </div>
   );
 };
