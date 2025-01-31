81f9a317b798b6eaf32b140b5f1b522ae66578ab
gternovs <gternovs@akamai.com>
2025-01-31 17:01:29 +0200

    Changed navbar style

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -7,7 +7,7 @@ const PostsPage: React.FC = () => {
   return (
     <div>
       <Navbar />
-      <div style={{ marginTop: "120px" }}>  {/* Adjust this value depending on Navbar height */}
+      <div style={{ marginTop: "140px" }}>  {/* Adjust this value depending on Navbar height */}
         <Posts />
       </div>
       <Footer />
