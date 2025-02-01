74ea7f436a861580c4f29ca04cd9b0228c53a337
gabimatatov <gavriel.matatov@veeva.com>
2025-02-01 18:37:48 +0200

    Configured api client to use access token

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from '../hooks/useAuth/AuthContext';
 import { Navigate } from 'react-router-dom';
+import Navbar from '../components/Navbar/Navbar';
 
 const ProtectedPage = () => {
   const { user, isAuthenticated, loading } = useAuth();
@@ -13,27 +14,30 @@ const ProtectedPage = () => {
   }
 
   return (
-    <div style={{ textAlign: "center", padding: "2rem" }}>
-      <h1>Welcome, {user?.username}</h1>
-      <h4>{user?.email}</h4>
-      <h5>{user?._id}</h5>
-      <p>This is a protected page. Only logged-in users can see this.</p>
+    <>
+      <Navbar />
+      <div style={{ textAlign: "center", padding: "2rem" }}>
+        <h1>Welcome, {user?.username}</h1>
+        <h4>{user?.email}</h4>
+        <h5>{user?._id}</h5>
+        <p>This is a protected page. Only logged-in users can see this.</p>
 
-      <button
-        style={{
-          padding: "10px 20px",
-          backgroundColor: "#ff4d4d",
-          color: "white",
-          border: "none",
-          borderRadius: "5px",
-          cursor: "pointer",
-          fontSize: "16px",
-          marginTop: "20px",
-        }}
-      >
-        Logout
-      </button>
-    </div>
+        <button
+          style={{
+            padding: "10px 20px",
+            backgroundColor: "#ff4d4d",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+            fontSize: "16px",
+            marginTop: "20px",
+          }}
+        >
+          Logout
+        </button>
+      </div>
+    </>
   );
 };
 
