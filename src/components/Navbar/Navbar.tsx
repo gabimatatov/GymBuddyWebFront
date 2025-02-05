831ad29edda91c6b6ef43b7a4c0d04a31c3294bd
gabimatatov <gavriel.matatov@veeva.com>
2025-02-05 21:01:40 +0200

    Added logout implementaion

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import { useAuth } from '../../hooks/useAuth/AuthContext';
@@ -7,12 +7,20 @@ const Navbar: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { logout } = useAuth();
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
 
-  const handleLogout = async () => {
+  const handleLogoutClick = () => {
+    setModalVisible(true);
+  };
+
+  const cancelLogout = () => {
+    setModalVisible(false);
+  };
+
+  const confirmLogout = async () => {
     try {
       logout();
-
-      // After logout, redirect to login page
+      setModalVisible(false);
       navigate('/login');
     } catch (error) {
       console.error('Error logging out', error);
@@ -52,14 +60,27 @@ const Navbar: React.FC = () => {
         </li>
       </ul>
 
-      {/* Right side: Logout (Redirects to Login) */}
+      {/* Right side: Logout */}
       <ul className={styles.authLinks}>
         <li>
-          <button onClick={handleLogout} className={`${styles.logout} ${styles.log}`}>
+          <button onClick={handleLogoutClick} className={`${styles.logout} ${styles.log}`}>
             Logout
           </button>
         </li>
       </ul>
+
+      {/* Logout Confirmation Modal */}
+      {modalVisible && (
+        <div className={`${styles["modal-overlay"]} ${modalVisible ? styles.show : ""}`}>
+          <div className={`${styles["modal-container"]} ${modalVisible ? styles.show : ""}`}>
+            <div className={styles["modal-title"]}>Are you sure you want to logout?</div>
+            <div className={styles["modal-buttons"]}>
+              <button className={`${styles["modal-button"]} ${styles.cancel}`} onClick={cancelLogout}>Cancel</button>
+              <button className={`${styles["modal-button"]} ${styles.confirm}`} onClick={confirmLogout}>Yes</button>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
