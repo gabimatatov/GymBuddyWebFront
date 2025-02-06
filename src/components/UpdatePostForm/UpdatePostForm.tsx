9218a2f9c70e6ec121728cb639566d7c737d6443
gternovs <gternovs@akamai.com>
2025-02-06 13:31:14 +0200

    Fixed remove image bug

--- src/components/UpdatePostForm/UpdatePostForm.tsx
+++ src/components/UpdatePostForm/UpdatePostForm.tsx
@@ -21,6 +21,7 @@ const UpdatePostForm: FC<UpdatePostFormProps> = ({ _id }) => {
     const [content, setContent] = useState<string>('');
     const [imageFile, setImageFile] = useState<File | null>(null);
     const [removeImage, setRemoveImage] = useState<boolean>(false);  // State to track image removal
+    const [previousImage, setPreviousImage] = useState<string | null>(null); // Track previous image URL
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -41,7 +42,7 @@ const UpdatePostForm: FC<UpdatePostFormProps> = ({ _id }) => {
 
           const imageUrl = post.image ? `http://localhost:3000${post.image}` : '/public/GymBuddyLogo.png';
           setPreviewImage(imageUrl);
-
+          setPreviousImage(imageUrl); // Set the previous image
           setTitle(post.title || '');
           setContent(post.content || '');
         } catch (error: unknown) {
@@ -56,25 +57,28 @@ const UpdatePostForm: FC<UpdatePostFormProps> = ({ _id }) => {
       fetchPost();
     }, [postId]);
 
+    // Handle file removal (clear input and revert to the previous image)
+    const handleRemoveFile = () => {
+      // Only proceed if an image is already uploaded or previewed
+      if (imageFile || previewImage === '/public/GymBuddyLogo.png') {
+        if (inputFileRef.current) {
+          inputFileRef.current.value = ""; // Clear the file input
+        }
+        setPreviewImage(previousImage); // Revert to the previous image
+        setImageFile(null); // Clear the image file state
+        setRemoveImage(false); // Reset the removal flag
+      }
+    };
+
     // Handle file selection and update the preview image
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       if (event.target.files && event.target.files.length > 0) {
         const file = event.target.files[0];
         const imageUrl = URL.createObjectURL(file);
-        setPreviewImage(imageUrl);
+        setPreviewImage(imageUrl); // Set preview image
         setImageFile(file); // Set the selected file to state
         setRemoveImage(false); // Reset remove image flag when a new image is selected
       }
-    };
-
-    // Handle file removal (clear input and preview)
-    const handleRemoveFile = () => {
-      if (inputFileRef.current) {
-        inputFileRef.current.value = "";
-      }
-      setPreviewImage(null);
-      setImageFile(null); // Remove the file from state
-      setRemoveImage(false); // Reset the removal flag when file is removed
     };
 
     const onSubmit = async () => {
@@ -206,6 +210,5 @@ const UpdatePostForm: FC<UpdatePostFormProps> = ({ _id }) => {
       </div>
     );
 };
-
 
 export default UpdatePostForm;
