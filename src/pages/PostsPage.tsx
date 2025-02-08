01ae0d6a85a65cf557d04c4d753d75ca3cb8e15b
gabimatatov <gavriel.matatov@veeva.com>
2025-02-08 23:35:13 +0200

    Added headline to explore page

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -21,6 +21,29 @@ const PostsPage: React.FC = () => {
     <div className={styles.postsPageWrapper}>
       <Navbar />
       <div className={styles.postsContainer}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+            width: "100%",
+          }}
+        >
+          <h1 style={{
+            marginBottom: '15px',
+            marginTop: '0px'
+          }}>Welcome to the GymBuddy Community!</h1>
+          <p style={{
+            marginTop: '0px',
+          }}>
+            Connect with fellow fitness enthusiasts, share your progress, and stay motivated!
+            <br />
+            Post your workouts, ask questions, and support each other on your fitness journey.
+          </p>
+        </div>
+
         <Posts />
       </div>
     </div>
